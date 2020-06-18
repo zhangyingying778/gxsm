7824c88a1a8e1ecc8b53556c382b8e753b1e5ea6
zhangyingying778 <z2169633352@126.com>
2020-06-18 11:07:37 +0800

    更新内容

---
+++ 第1章/listing_1-1.py
@@ -0,0 +1,10 @@
+listing_1-1.py
+@@ -1,5 +1,8 @@
+#第1个程序
+print "I love pizza!"
+print "pizza " * 20
+print "yum " * 40
+print "I'm full."
++
++
+#这里主要就使用了一个print函数来显示字符串内容
---
+++ 第6章/listing_6-3.py
@@ -0,0 +1,5 @@
+# EasyGui - Ice Cream choices using an EnterBox
+
+import easygui
+flavor = easygui.enterbox("What is your favorite ice cream flavor?")
+easygui.msgbox ("You entered " + flavor)
